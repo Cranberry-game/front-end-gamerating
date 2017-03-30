6fc55891b42e0291490bb7df6fcbbf803ae1902f
Pimino <bushang.zhan@gmail.com>
2017-03-30 16:57:23 -0400

    add several scss files

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -6,6 +6,7 @@ import { Tabs, Tab } from 'material-ui/Tabs'
 import SwipeableViews from 'react-swipeable-views'
 import SearchGameLists from './SearchGameLists'
 import SearchGames from './SearchGames'
+import '../../css/components/Search.scss'
 
 /*export default class Search extends Component {
     constructor(props) {
